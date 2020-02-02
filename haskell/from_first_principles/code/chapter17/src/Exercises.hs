74d20b21cad3e9171e5d08194da8fb17d6c1756d
hendriko373 <jennen.hendrik@gmail.com>
2020-02-02 21:08:24 +0100

    Add Two a instance to exercises

--- haskell/from_first_principles/code/chapter17/src/Exercises.hs
+++ haskell/from_first_principles/code/chapter17/src/Exercises.hs
@@ -23,3 +23,24 @@ instance Eq a => EqProp (Pair a) where
     (=-=) = eq
 
 testPair = quickBatch $ applicative (undefined :: Pair (String, String, Int))
+
+-- 2
+data Two a b = Two a b deriving (Show, Eq)
+
+instance Functor (Two a) where
+    fmap f (Two a b) =  Two a $ f b
+
+instance Monoid a => Applicative (Two a) where
+    pure = Two mempty
+    (Two a1 f) <*> (Two a2 b) = Two (a1 <> a2) $ f b
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Two a b) where
+    arbitrary = do
+        a <- arbitrary
+        b <- arbitrary
+        return $ Two a b
+
+instance (Eq a, Eq b) => EqProp (Two a b) where
+    (=-=) = eq
+
+testTwo = quickBatch $ applicative (undefined :: Two String (String, String, Int))
