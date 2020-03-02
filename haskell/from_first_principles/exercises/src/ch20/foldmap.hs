84aaae5a0bbd27c116a7f5aff025730cdf62cec7
Hendrik Jennen <jennen.hendrik@gmail.com>
2020-03-02 21:51:29 +0100

    Update foldmap.hs

--- haskell/from_first_principles/exercises/src/ch20/foldmap.hs
+++ haskell/from_first_principles/exercises/src/ch20/foldmap.hs
@@ -1,9 +1,20 @@
+newtype Endo b = Endo
+  { appEndo :: (b -> b)
+  }
+
+instance Monoid (Endo b) where
+    mempty = Endo id
+    mappend (Endo f) (Endo g) = Endo $ f . g
+
 data List a =
     End
   | Cons a (List a)
 
 instance Foldable List where
-    foldmap :: Monoid m => (a -> m) -> List a -> m
-    foldmap _ End         = mempty
-    foldmap f (Cons a as) = (f a) <> foldmap f as 
+    foldMap :: Monoid m => (a -> m) -> List a -> m
+    foldMap _ End         = mempty
+    foldMap f (Cons a as) = (f a) <> foldmap f as 
+
+    foldr f z xs = appEndo (foldmap (Endo . f) xs) z
+
 
