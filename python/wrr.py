1093a47a4ff51ed54e003d3947a069e93e494718
Hendrik Jennen <hendrik.jennen@arcelormittal.com>
2020-04-06 16:42:30 +0200

    Simplify algorithm

--- python/wrr.py
+++ python/wrr.py
@@ -7,26 +7,20 @@ def deadlines(loads, priors):
     using weighted round robin to schedule the incoming loads.
     """
     result = [0] * len(loads)
-    done = [0] * len(loads)
-    #nonzero = [p if p != 0 else 0.01 for p in priors]
-    nonzero = priors
+    deadlines = [0] * len(loads)
 
     time = 0
-    while any (not is_finished(t[0], t[1]) for t in zip(done, loads)):
-        left = [t[1] - t[0] for t in zip(done, loads)]
-        normal = normalized((pushforward(nonzero, done, loads)))
+    while any (l > 0 for l in loads):
+        normal = normalized (pushforward (priors, loads))
         step = min ([t[0] / t[1] \
-            for t in list (filter (lambda t: t[1] > 0, zip (left, normal)))])
-        done = [t[0] + t[1]*step for t in zip(done, normal)]
-        time = time + step
-        result = [
-                time if is_finished(t[0], t[1]) and t[2] == 0 else t[2] 
-                for t in zip(done, loads, result)]
+            for t in list (filter (lambda t: t[1] > 0, zip (loads, normal)))])
+        deadlines = [t[0] + step if t[1] > 0 else t[0] for t in zip(deadlines, loads)]
+        loads = [t[0] - t[1] * step if t[0] > 0 else t[0] for t in zip(loads, normal)]
 
-    return [round(r, 0) for r in result]
+    return [round (d, 0) for d in deadlines]
 
-def pushforward(priors, done, loads):
-    return [0 if is_finished(t[0], t[1]) else t[2] for t in zip(done, loads, priors)]
+def pushforward(priors, loads):
+    return [t[1] if t[0] > 0  else 0 for t in zip(loads, priors)]
 
 def normalized(priors):
     return [p / sum(priors) for p in priors]
